d06794fc001c1af436586f06a55d524d02b7b521
Hein123456 <heinrichenslin91@gmail.com>
2023-09-17 23:31:04 +0200

    changes to JS, mobile

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 // Function to check if an element is in viewport
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  // Function to add animation class to sections in viewport
-  function animateSections() {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach((section) => {
-      if (isInViewport(section)) {
-        section.classList.add('animated');
-      }
-    });
-  }
-  
-  // Event listener for scroll event
-  window.addEventListener('scroll', () => {
-    animateSections();
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to add animation class to sections in viewport
+function animateSections() {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section) => {
+    if (isInViewport(section)) {
+      section.classList.add('animated');
+    }
   });
-  
-  // Initial animation check on page load
-  animateSections();
+}
+
+// Event listeners for scroll and resize events
+window.addEventListener('scroll', () => {
+  requestAnimationFrame(animateSections);
+});
+window.addEventListener('resize', () => {
+  requestAnimationFrame(animateSections);
+});
+
+// Initial animation check on page load
+requestAnimationFrame(animateSections);
