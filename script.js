48cde95d7c22228d75afbb7d3d523ef098029579
Hein123456 <heinrichenslin91@gmail.com>
2023-09-17 23:37:29 +0200

    JS fix test 1

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
+    rect.top >= 10 &&
+    rect.left >= 10 &&
     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
